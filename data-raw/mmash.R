2562ff31a455f472cd00517442bcffe5db350440
Luke W. Johnston <lwjohnst@gmail.com>
2021-06-29 15:54:01 +0200

    moved processing code into script and create dataset

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -1,19 +1,74 @@
+library(tidyverse)
+library(vroom)
+library(fs)
 library(here)
+source(here("R/functions.R"))
 
 # Download
 # mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
 # download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
 
 # Unzip
-unzip(here("data-raw/mmash-data.zip"),
-      exdir = here("data-raw"),
-      junkpaths = TRUE)
-unzip(here("data-raw/MMASH.zip"),
-      exdir = here("data-raw"))
+# unzip(here("data-raw/mmash-data.zip"),
+#       exdir = here("data-raw"),
+#       junkpaths = TRUE)
+# unzip(here("data-raw/MMASH.zip"),
+#       exdir = here("data-raw"))
 
 # Remove/tidy up left over files
-library(fs)
-file_delete(here(c("data-raw/MMASH.zip",
-                   "data-raw/SHA256SUMS.txt",
-                   "data-raw/LICENSE.txt")))
-file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
+# library(fs)
+# file_delete(here(c("data-raw/MMASH.zip",
+#                    "data-raw/SHA256SUMS.txt",
+#                    "data-raw/LICENSE.txt")))
+# file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
+
+user_info_df <- import_multiple_files(
+    "user_info.csv",
+    import_user_info
+)
+saliva_df <- import_multiple_files(
+    "saliva.csv",
+    import_saliva
+)
+rr_df <- import_multiple_files(
+    "RR.csv",
+    import_rr
+)
+actigraph_df <- import_multiple_files(
+    "Actigraph.csv",
+    import_actigraph
+)
+
+summarised_rr_df <- rr_df %>%
+    group_by(user_id, day) %>%
+    summarise(across(ibi_s,
+                     list(mean = mean, sd = sd),
+                     na.rm = TRUE)) %>%
+    ungroup()
+
+summarised_actigraph_df <- actigraph_df %>%
+    group_by(user_id, day) %>%
+    summarise(across(
+        hr, list(mean = mean, sd = sd),
+        na.rm = TRUE
+    )) %>%
+    ungroup()
+
+saliva_with_day_df <- saliva_df %>%
+    mutate(day = case_when(
+        samples == "before sleep" ~ 1,
+        samples == "wake up" ~ 2,
+        TRUE ~ NA_real_
+    ))
+
+mmash <- reduce(
+    list(
+        user_info_df,
+        saliva_with_day_df,
+        summarised_rr_df,
+        summarised_actigraph_df
+    ),
+    full_join
+)
+
+usethis::use_data(mmash, overwrite = TRUE)
